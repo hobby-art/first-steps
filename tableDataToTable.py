3cc04d77987554bcba36a4aa791dc458e55b700f
hobby-art <ramzes.jr@gmail.com>
2026-01-10 21:25:47 +0200

    Update tableDataToTable.py
    added comments

--- tableDataToTable.py
+++ tableDataToTable.py
@@ -1,3 +1,4 @@
+# Table data to be used for formating.
 tableData = [
     ["apples", "oranges", "cherries", "banana"],
     ["Alice", "Bob", "Carol", "David"],
@@ -7,15 +8,18 @@ tableData = [
 
 def printTable(table):
 
+    # Define how much we need to justify for each column.
     colWidths = [0] * len(table)
-
+    # Simply compare the length of the next item of a list with the previous item. Loop for each column (sublist).
     for i, sub_list in enumerate(table):
         for item in sub_list:
             if len(item) > colWidths[i]:
                 colWidths[i] = len(item)
 
-    num_rows = len(table[0])
 
+    # Define how many rows we need.
+    num_rows = len(table[0])
+    # Print the first item of each sublist in one row, then the second, and so forth.
     for row_index in range(num_rows):
         for i, sub_list in enumerate(table):
             print(sub_list[row_index].rjust(colWidths[i]), end=" ")
